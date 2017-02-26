5d34d87254380685ca6eefa4ba692bb9dd711a04
05K4R1N <oskr.cbba2010@gmail.com>
2017-02-26 18:20:52 -0400

    [M] PROBANDO

--- main.java
+++ main.java
@@ -13,6 +13,28 @@ public class main{
                 String[][] tablero = jugada.dibujarCampo(t);
                 Boolean[][] estados = jugada.inicializarEstado();
                 jugada.imprimirTablero(tablero);
+                System.out.print("NIVEL DE JUEGO: [1] FACIL [2] MEDIO [3] MUY DIFICIL: ");
+                int nivel = lector.nextInt();
+                jugada.colocarMinas(nivel);
+                System.out.println("INICIANDO...");
+                while(true){
+                    System.out.print("X: ");
+                    int x = lector.nextInt();
+                    System.out.print("Y: ");
+                    int y = lector.nextInt();
+                    if(jugada.mina_detectada(x, y)){
+                        tablero[x][y] = "X";
+                        jugada.imprimirTablero(tablero);
+                        break;
+                    }
+                    tablero[x][y] = "O";
+                    jugada.imprimirTablero(tablero);
+                }
+        }else{
+            System.out.println("PROGRAMA FINALIZADO");
         }
+        /*Jugada j = new Jugada(10);
+        Boolean[][] campo = j.inicializarEstado();
+        j.colocarMinas(3);*/
     }
 }
